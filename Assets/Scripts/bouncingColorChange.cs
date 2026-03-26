d2ae824e44c3a090d1f3d6a35723dcd451b0cb8c
vincent-tarrit <vincent@tarrit.com>
2026-03-26 20:37:51 +0100

    debug: changing color on bounce

--- Assets/Scripts/bouncingColorChange.cs
+++ Assets/Scripts/bouncingColorChange.cs
@@ -2,13 +2,38 @@ using UnityEngine;
 
 public class bouncingColorChange : MonoBehaviour
 {
+
+    // Kylian
+//    [SerializeField] private Renderer visualsRenderer;
+//    void OnTriggerEnter(Collider collision)
+//    {
+//        Debug.Log("GameObject collided with " + collision.name);
+//
+//        Renderer renderer = GetComponent<Renderer>();
+//
+//        renderer.material.color = Random.ColorHSV();
+//    }
+    
+    // VINC
     [SerializeField] private Renderer visualsRenderer;
-    void OnTriggerEnter(Collider collision)
+    void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("GameObject collided with " + collision.name);
+        Debug.Log("GameObject collided with " + collision.gameObject.name);
 
-        Renderer renderer = GetComponent<Renderer>();
+         if (visualsRenderer != null)
+        {
+            Debug.Log("Changing color of " + visualsRenderer);
 
-        renderer.material.color = Random.ColorHSV();
+            Material mat = visualsRenderer.material;
+
+            if (mat.HasProperty("_BaseColor"))
+            {
+                mat.SetColor("_BaseColor", Random.ColorHSV());
+            }
+            else
+            {
+                mat.color = Random.ColorHSV();
+            }
+        }
     }
 }
